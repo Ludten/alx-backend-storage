292662097400498f6966b9ba22efd7ca4386bd3e
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-12 15:46:11 +0100

    MySQL advanced
    Checker ish

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands with Glam rock as their main style
-SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', CONCAT_WS("", style)) > 0
-    ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, YEAR(NOW())) - formed) AS "lifespan"
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', CONCAT_WS("", style)) > 0
+ORDER BY lifespan DESC;
