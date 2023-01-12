bd512a4dca1b8be24f9131b8b6d91c1763cc5bc0
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-12 15:42:57 +0100

    MySQL advanced
    This is ridiculous

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- list all bands with Glam rock as their main style
-SELECT band_name, (COALESCE(split, YEAR(NOW())) - formed) AS "lifespan" FROM metal_bands
+SELECT band_name, (IFNULL(split, YEAR(NOW())) - formed) AS "lifespan"
+FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', CONCAT_WS("", style)) > 0
 ORDER BY lifespan DESC;
