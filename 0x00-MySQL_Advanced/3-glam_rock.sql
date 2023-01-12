383c77b4a7fee1e2547c7a38c3ea285c1c4f5356
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-12 15:38:38 +0100

    MySQL advanced
    shouldn't results matter more

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
 -- list all bands with Glam rock as their main style
-SELECT band_name, COALESCE(split, 2020)-formed as "lifespan" FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-GROUP BY band_name, lifespan
+SELECT band_name, COALESCE(split, YEAR(NOW())) - formed as "lifespan" FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', CONCAT_WS("", style)) > 0
 ORDER BY lifespan DESC;
