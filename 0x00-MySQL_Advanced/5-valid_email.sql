2c6b71c4de67a0c1c33a2bf6044d4e39de3ea101
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-12 19:45:24 +0100

    MySQL advanced
    Spacing for task-5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
--- create a trigger that resets the attribute valid_email only when the email has been changed
+-- create a trigger that resets the attribute valid_email
+-- only when the email has been changed
 DROP TRIGGER IF EXISTS valid;
 DELIMITER //
 CREATE TRIGGER valid
-BEFORE UPDATE
-ON users FOR EACH ROW
+BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF NEW.email != OLD.email THEN
+	IF OLD.email != NEW.email THEN
 		SET NEW.valid_email = ABS(OLD.valid_email - 1);
 	END IF;
 END //
