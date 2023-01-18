58e7a315efcfaf21a364b75c2681e0f651eb8d2f
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-19 00:18:53 +0100

    Redis basic
    Ma yo mi le nu

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis module
 """
 
 import redis
-from typing import Union
+from typing import Optional, Union, Callable
 from uuid import uuid4
 
 
@@ -27,3 +27,24 @@ class Cache:
         key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Union[Callable[[Union[bytes, None]],
+                                           Union[bytes, None, bytes]],
+                                  None] = None
+    ) -> Union[Optional[bytes], bytes]:
+        if fn is not None:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> Optional[bytes]:
+        """
+        Get a int variable from redis
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Optional[bytes]:
+        """
+        Get a int variable from redis
+        """
+        return self.get(key, int)
